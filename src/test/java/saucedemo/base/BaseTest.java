6465c8a39c5825905b6db2693c1f7e5eeb99a208
Slobodan Kostic <stickopn@gmail.com>
2026-02-27 18:04:59 +0100

    Komit zbog ponovnog okidanja Actions-2

--- src/test/java/saucedemo/base/BaseTest.java
+++ src/test/java/saucedemo/base/BaseTest.java
@@ -29,10 +29,18 @@ public abstract class BaseTest {
         options.addArguments("--incognito");
         options.addArguments("--disable-notifications");
 
-        options.addArguments("--headless=new");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--window-size=1920,1080");
+        // ako je CI (GitHub Actions automatski setuje CI=true)
+        boolean isCI = "true".equalsIgnoreCase(System.getenv("CI"));
+
+        if (isCI) {
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--window-size=1920,1080");
+        } else {
+            // lokalno: može maximize ili start-maximized
+            options.addArguments("--start-maximized");
+        }
 
         // ugasi password manager + leak detection popup
         Map<String, Object> prefs = new HashMap<>();
@@ -42,7 +50,12 @@ public abstract class BaseTest {
         options.setExperimentalOption("prefs", prefs);
 
         driver = new ChromeDriver(options);
-        driver.manage().window().maximize();
+
+        // BITNO: ne maximize u CI/headless
+        if (!isCI) {
+            driver.manage().window().maximize(); // može i da izbrišeš skroz jer imaš --start-maximized
+        }
+
         driver.get("https://www.saucedemo.com/");
     }
 
